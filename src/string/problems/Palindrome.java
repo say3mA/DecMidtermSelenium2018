ea5559b04e6e18d02adc65225900e2b61380229c
Mac User <MacUser@Mac-Users-MacBook-138.local>
2018-12-23 15:07:50 -0500

    did a little more

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -7,5 +7,17 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+
+    }
+    public static boolean isPalindrome(String s){
+        String a = "";
+        for(int i = s.length()-1; i >=0; i--){
+            a = a + s.charAt(i);
+        }
+        if(s.equals(a)){
+            return true;
+
+        }
+        else return false;
     }
 }
